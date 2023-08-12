5570fe43ef5bffad884c0104d429e4ff576b4253
arakinoko <strawberry.candy16@gmail.com>
2023-08-12 16:31:28 +0900

    ActiveStorageを導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+
+  private
+
+  def items_params
+    params.require(:item).permit(:name, :description, :status_id, :postage_id, :prefecture_id, :shopping_date_id, :price, :category_id, :image).merge(user_id: current_user.id)
+  end
 end
