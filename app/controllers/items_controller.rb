bf292b7be424548058388a20ab94b0ea8be2ac7d
arakinoko <strawberry.candy16@gmail.com>
2023-08-13 20:08:16 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,25 @@
 class ItemsController < ApplicationController
 
+  def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
-  def items_params
+  def item_params
     params.require(:item).permit(:name, :description, :status_id, :postage_id, :prefecture_id, :shopping_date_id, :price, :category_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,33 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :shopping_date
+  belongs_to :category
+
+
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :status_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :shopping_date_id
+    validates :price
+    validates :category_id
+    validates :user_id
+  end
+# ジャンルの選択が「--」の時は保存不可
+  with_options numericality: { other_than: 0,  message: "can't be blank"} do
+    validates :category_id
+    validates :prefecture_id
+    validates :status_id
+    validates :postage_id
+    validates :shopping_date_id
+  end
+ 
+
 end
