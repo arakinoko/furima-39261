955abe0c3ca8e0d163801025360df6facadd3071
arakinoko <strawberry.candy16@gmail.com>
2023-08-20 16:07:52 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :update, :show]
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
   before_action :check_item_ownership, only: [:edit, :update]
 
   def index
@@ -35,7 +35,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     if @item.user == current_user
       @item.destroy
     end
